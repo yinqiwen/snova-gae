175c952e1ab87e61355ff98c5f12a903a65adf4e
yinqiwen <yinqiwen@gmail.com>
2013-03-17 14:30:57 +0800

    rm old deployer

--- java/deploy/AppCfgWrapper/appcfgwrapper.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-@javaw -jar "%~dp0\AppCfgWrapper.jar" %*
--- java/deploy/install.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set APP_LOCATION=%~dp0\war
-@start javaw "-Dappengine.app.location=%APP_LOCATION%" -jar  AppCfgWrapper\AppCfgWrapper.jar %*
